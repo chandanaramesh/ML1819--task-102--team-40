11d02f2c4398e2d5240e4fa47c22c39bae82df56
Suprith <44401046+SuprithRamesh@users.noreply.github.com>
2018-10-26 16:04:14 +0100

    Update datapre.py
    Added SVM with RBF regression model code for Analysis of data

--- datapre.py
+++ datapre.py
@@ -3,12 +3,14 @@
 """
 Created on Tue Oct 23 12:19:18 2018
 
-@author: rameshc
+@author: rameshsu
 """
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
+import pandas as pd
+from sklearn import svm
+from sklearn import metrics
+       
 dataset = pd.read_csv(r'energydata.csv', delimiter=',')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, -1].values
@@ -27,3 +29,37 @@ from sklearn.preprocessing import MinMaxScaler
 mm_X = MinMaxScaler()
 X_train = mm_X.fit_transform(X_train)
 X_test = mm_X.transform(X_test)
+
+#Create a svm Classifier
+#clf = svm.SVC(kernel='linear') # Linear Kernel
+
+clf = svm.SVC(kernel='rbf', random_state=0, gamma=0.01, C=1) # Radial Model
+
+#Train the model using the training sets
+clf.fit(X_train, Y_train)
+
+#Predict the response for test dataset
+Y_pred = clf.predict(X_test)
+
+accuracy=metrics.accuracy_score(Y_test, Y_pred)
+print("Accuracy:",accuracy)
+#print("Precision:",metrics.precision_score(Y_test, Y_pred))
+#print("Recall:",metrics.recall_score(Y_test, Y_pred))
+
+plt.style.use('seaborn-whitegrid')
+#plt.set_xlim(0, 1.5)
+plt.title('Predicted Values')
+plt.xlabel('Appliance')
+plt.ylabel('Value')
+
+plt.plot(Y_test,'g^')
+plt.plot(Y_pred,'r^')
+plt.savefig('Graph_PredictedValue_Gamma.png')
+plt.show()
+
+#plt.title('Test Values')
+#plt.xlabel('Appliance Index')
+#plt.ylabel('Value')
+#plt.plot(Y_test,color='red')
+#plt.show()
+#plt.savefig('Graph_TestValue.png')
