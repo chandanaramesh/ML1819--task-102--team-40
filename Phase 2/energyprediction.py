d70ca7df08376bc01f576ca264d1b152d6a32cfb
chandanaramesh <chandanaramesh.6683@gmail.com>
2018-12-18 21:48:07 +0000

    Update energyprediction.py
    Correlation Matrix

--- Phase 2/energyprediction.py
+++ Phase 2/energyprediction.py
@@ -22,3 +22,25 @@ from sklearn.preprocessing import MinMaxScaler
 mm_X = MinMaxScaler()
 X_train = mm_X.fit_transform(X_train)
 X_test = mm_X.transform(X_test)
+
+#Correlation Matrix
+dataset=dataset[0:30]
+correlations = dataset.corr()
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 12
+fig_size[1] = 12
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(correlations, vmin=-1, vmax=1)
+fig.colorbar(cax)
+ticks = np.arange(0,30,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.grid(True)
+plt.title('Feature Correlation')
+labels =['date', 'lights', 'T1', 'RH_1', 'T2', 'RH_2', 'T3', 'RH_3', 'T4', 'RH_4', 'T5', 'RH_5', 'T6', 'RH_6', 'T7', 'RH_7', 'T8', 'RH_8', 'T9', 'RH_9', 'T_out', 'Press', 'RH_out', 'Wind', 'Vis', 'Tdew', 'rv1', 'rv2', 'energy']
+ax.set_xticklabels(labels,fontsize=6)
+ax.set_yticklabels(labels,fontsize=6)
+ax.set_xticklabels(labels)
+ax.set_yticklabels(labels)
+plt.show()
